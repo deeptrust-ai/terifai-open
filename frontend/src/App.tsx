b9637939f85a8265fa343ad6c9db5d89afa2900e
shuaibahmed01 <121910026+shuaibahmed01@users.noreply.github.com>
2025-01-15 18:06:28 -0800

    coming soon message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -239,6 +239,9 @@ export default function App() {
         <CardFooter className="flex flex-col gap-2">
           <div className="flex gap-3 w-full">
             <div className="flex-1">
+              <div className="text-xs text-primary font-bold text-center mb-1.5 invisible">
+                (Coming Soon 🤫)
+              </div>
               <Button
                 fullWidthMobile
                 size="lg"
@@ -252,6 +255,9 @@ export default function App() {
               </p>
             </div>
             <div className="flex-1">
+              <div className="text-xs text-primary font-bold text-center mb-1.5">
+                (Coming Soon 🤫)
+              </div>
               <Button
                 fullWidthMobile
                 size="lg"
