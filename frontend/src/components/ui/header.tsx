b5183d4d9c576a831e20701b592e60645491afbf
shuaibahmed01 <121910026+shuaibahmed01@users.noreply.github.com>
2025-01-23 23:32:06 -0800

    added deeptrust logo

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -1,8 +1,5 @@
-import Logo from "../logo";
+import deeptrust from "../../assets/logos/deeptrust.png";
 import ExpiryTimer from "../Session/ExpiryTimer";
-
-const aCx =
-  "underline decoration-primary-400/0 hover:decoration-primary-400 underline-offset-4 transition-all duration-300";
 
 function Header() {
   return (
@@ -11,18 +8,13 @@ function Header() {
       className="w-full flex self-start items-center p-[--app-padding] justify-between"
     >
       <div className="group flex gap-8">
-        <span className="border border-primary-200 rounded-xl p-2 flex place-content-center transition-all bg-white shadow-short hover:shadow-mid">
-          <Logo className="w-[42px] h-auto aspect-square [&>*:nth-child(5)]:invisible group-hover:[&>*:nth-child(5)]:visible group-hover:[&>*:nth-child(4)]:invisible group-hover:animate-wiggle" />
-        </span>
-
-        <nav className="pointer-events-none flex-row items-center gap-8 text-lg leading-7 hidden group-hover:flex group-hover:pointer-events-auto">
-          <a href="https://git.new/ai" target="_blank" className={aCx}>
-            GitHub
-          </a>
-          <a href="https://discord.gg/pipecat" target="_blank" className={aCx}>
-            Discord
-          </a>
-        </nav>
+        <a 
+          href="https://deeptrust.ai" 
+          rel="noopener noreferrer"
+          className="flex place-content-center transition-all"
+        >
+          <img src={deeptrust} alt="TerifAI Logo" className="w-auto h-10" />
+        </a>
       </div>
       <ExpiryTimer />
     </header>
